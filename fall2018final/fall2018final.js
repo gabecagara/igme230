d89858c42b9b7ea4369d163e856ab37deee44178
Gabriel C <Gabe@Rinas-MBP.wireless.rit.edu>
2018-12-06 18:26:40 -0500

    update

--- fall2018final/fall2018final.js
+++ fall2018final/fall2018final.js
@@ -1,16 +1,16 @@
-$('input: radio[name=mainmenu]').click(function() {
+$('.menu').click(function() {
     $(this).find("submenu").slideToggle(); 
 });
 
 let selection = ("article0.txt")   /*making the selction to be this text file on default.  */
 
 /*happens on load, */
-        $('form').val(selection); /* gets the value of the dropdown and loads your name to that dropdown menu eleemnt by default. CHOOSECONTENT IS THE OPTION ON THE DROPDOWN */
-        $('article').load(selection);  /* changes article HEADER to content found in text file. CONTENT DISPLAYS THE TEXT FROM THE TEXT FILE AS THE HEADER IN THE PAGE */
+        $("form").val(selection); /* gets the value of the dropdown and loads your name to that dropdown menu eleemnt by default. CHOOSECONTENT IS THE OPTION ON THE DROPDOWN */
+        $("article").load(selection);  /* changes article HEADER to content found in text file. CONTENT DISPLAYS THE TEXT FROM THE TEXT FILE AS THE HEADER IN THE PAGE */
 
         $('input: radio[name=content]').change(function() {
             selection = $(this).val();  /* reassigns the content value to the current and new dropdown value  */
-            $('input: radio[name=content]').load(selection); /*loads content in */
+            $("article").load(selection); /*loads content in */
         });
 
 
